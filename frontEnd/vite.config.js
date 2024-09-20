16a5229816cfbe2c5b676ba71b3ccae98f409a33
TeddyLondon17 <alexanderstrife17@gmail.com>
2024-09-20 13:53:04 -0500

    new config

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  input: path.resolve(__dirname, 'src/index.html'), // Specify the path to your index.html
+  output: {
+    dir: path.resolve(__dirname, 'dist'),
+    format: 'es',
+  },
+});
